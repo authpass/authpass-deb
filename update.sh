609648f7f859b116745485f3a0127dd89a534c37
Herbert Poul <herbert@codeux.design>
2020-07-23 14:13:52 +0200

    update

--- update.sh
+++ update.sh
@@ -2,7 +2,24 @@
 
 set -xeu
 
-version=$(curl -s https://data.authpass.app/data/artifacts/authpass-linux-latest.tar.gz.txt)
-echo "version: $version"
+if ! test -d package ; then
+  echo "Launched from wrong directory."
+  exit 1
+fi
 
-echo dch -v ${version}
+artifacts='https://data.authpass.app/data/artifacts'
+
+version=$(curl -s ${artifacts}/authpass-linux-latest.tar.gz.txt)
+echo "version: $version"
+debversion=$(echo ${version} | tr _ +)
+debarchive="authpass_${debversion}.orig.tar.gz"
+
+if test -f "${debarchive}" ; then
+  echo "Using existing file: ${debarchive}"
+else
+  curl -o "${debarchive}" ${artifacts}/authpass-linux-${version}.tar.gz
+fi
+
+pushd package
+
+dch -M -v ${debversion} -D bionic "Update deb package."
